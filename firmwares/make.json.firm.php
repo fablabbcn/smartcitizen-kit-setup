f4adaafd5b147854906862dcd95eb3e29407dd08
Pral2a <guillem@pral2a.com>
2016-03-11 20:11:07 +0100

    Better gen and regeneration

--- firmwares/make.json.firm.php
+++ firmwares/make.json.firm.php
@@ -1,6 +1,6 @@
 <?php
 
-$projectName = "sck_beta_k.1.0_v.0.9.0.json";
+$projectName = "sck_beta_k.1.1_v.0.9.0";
 
 $arduinoCompilerELF= "/var/folders/0w/z91mcwks11b_r7x7rcd_bbbr0000gn/T/build7869240854402068297.tmp/sck_beta_v0_9.cpp.elf";
  
@@ -14,11 +14,13 @@ $localjsonfolder = "json";
 
 $jsonFile =  $localjsonfolder . "/". $projectName .".json";
 
+date_default_timezone_set("UTC"); 
+
 exec("$objCopyPath -R .eeprom -O binary $arduinoCompilerELF $binfile");
 
 $bin64 = base64_encode(file_get_contents($binfile));
 
-$firmJson = array('project' => $projectName, 'bin' => $bin64);
+$firmJson = array('project' => $projectName, 'generated' => date('Y-m-d\TH:i:s'), 'bin' => $bin64);
 
 $jsonFile = fopen($jsonFile, 'w');
 
